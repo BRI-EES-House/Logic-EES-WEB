294015e519597bc0e502c1e9960a26328840e2db
Hisashi Miura <hisashi.office@gmail.com>
2018-05-02 15:39:08 +0900

    Add Tests

--- _03_HCLoad_and_Envelope/EnvelopePerformanceSimpleUnitTest.py
+++ _03_HCLoad_and_Envelope/EnvelopePerformanceSimpleUnitTest.py
@@ -9,50 +9,45 @@ class TestSEPS(unittest.TestCase):
     def test_gen_includingQdashMhMc(self):
         
         test_patterns = [
-                ('region6', 'base_ins', 'bath_base_ins',  0.240, 0.530, 0.000, 0.480, 2.330, 3.490, 0.510, 0.510, 'yes', 1.0, 1.0, 'yes', 1.8, 1.8, 1.8, 2.8794288888888886, 0.94, 3.3, 3.1, 0.09495988888888889, 0.10108633333333332)
+                ( '1', 120.08, 29.81, 51.34, '1', '1', 7.7, 6.67, 5.27, 5.27, 4.65, 6.51, 0.7, 0.7, '1', 0.93, 0.51, 1.8, 1.8, 1.8, 6.14, 13.9, 14.7),
+                ( '6', 120.08, 29.81, 51.34, '1', '1', 7.7, 6.67, 5.27, 5.27, 4.65, 6.51, 0.7, 0.7, '1', 0.93, 0.51, 1.8, 1.8, 1.8, 6.14, 13.5, 14.4)
                 ]
-        for region, house_type, bath_ins_type, U_roof, U_wall, U_floorBath, U_floorOther, U_door, U_window, eta_d_cooling, eta_d_heating, fValue_useDefault, fValue_heating, fValue_cooling, psi_useDefault, psi_perimeterEntrance, psi_perimeterBath, psi_perimeterOther, expected_Qdash, expected_UA, expected_etaAH, expected_etaAC, expected_muC, expected_muH in test_patterns:
+        for region, area_total, area_main, area_other, house_type, bath_ins_type, U_roof, U_wall, U_floorOther, U_floorBath, U_door, U_window, eta_d_cooling, eta_d_heating, fValue_useDefault, fValue_cooling, fValue_heating, psi_perimeterOther, psi_perimeterEntrance, psi_perimeterBath, expected_UA, expected_etaAC, expected_etaAH in test_patterns:
             with self.subTest():
                 spec = {
-                        'house_type'    : house_type,
-                        'bath_ins_type' : bath_ins_type,
-                        'region'        : region,
+                        'region' : { '1':'region1', '2':'region2', '3':'region3', '4':'region4', '5':'region5', '6':'region6', '7':'region7', '8':'region8' }[region],
+                        'house_type' : { '1' : 'floor_ins', '2' : 'base_ins', '3' : 'floor_and_base_ins' }[house_type],
+                        'bath_ins_type' : { '1' : 'bath_floor_ins', '2' : 'bath_base_ins', '3' : 'bath_2nd_floor' }[bath_ins_type],
                         'U' : {
                                 'roof'         : U_roof,
                                 'wall'         : U_wall,
+                                'floorOther'   : U_floorOther,
+                                'floorBath'    : U_floorBath,
                                 'door'         : U_door,
                                 'window'       : U_window,
-                                'floorBath'    : U_floorBath,
-                                'floorOther'   : U_floorOther,
                                 },
                         'psi' : {
-                                'useDefaultEtrc' : psi_useDefault,
+                                'useDefaultEtrc' : 'no',
+                                'prmOther'       : psi_perimeterOther,
                                 'prmEtrc'        : psi_perimeterEntrance,
                                 'prmBath'        : psi_perimeterBath,
-                                'prmOther'       : psi_perimeterOther,
                                 },
                         'eta_d' : {
                                 'heating' : eta_d_heating,
                                 'cooling' : eta_d_cooling
                                 },
                         'fValue' : {
-                                'useDefault' : fValue_useDefault,
+                                'useDefault' : { '1' : 'yes', '2' : 'no' }[fValue_useDefault],
                                 'heating'    : fValue_heating,
                                 'cooling'    : fValue_cooling
                                 }
                         }
-                actual_Qdash = SEPS(spec)['Qdash']
                 actual_UA    = SEPS(spec)['UA']
                 actual_etaAH = SEPS(spec)['etaAH']
                 actual_etaAC = SEPS(spec)['etaAC']
-                actual_muH   = SEPS(spec)['muH']
-                actual_muC   = SEPS(spec)['muC']
-                self.assertAlmostEqual(actual_Qdash, expected_Qdash, delta = 0.000001)        
                 self.assertAlmostEqual(actual_UA,    expected_UA,    delta = 0.000001)
                 self.assertAlmostEqual(actual_etaAH, expected_etaAH, delta = 0.000001)
                 self.assertAlmostEqual(actual_etaAC, expected_etaAC, delta = 0.000001)
-                self.assertAlmostEqual(actual_muC,   expected_muC,   delta = 0.000001)
-                self.assertAlmostEqual(actual_muH,   expected_muH,   delta = 0.000001)
 
 
 if __name__ == "__main__":
